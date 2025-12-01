05071c7b80cb0401a959e8f412071395e2517273
mahadi40011 <mahadi40011@gmail.com>
2025-12-01 16:14:31 +0600

    Created get method for a single data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const admin = require("firebase-admin");
 const port = process.env.PORT || 3000;
 const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
@@ -66,9 +66,17 @@ async function run() {
 
     // get all data from database
     app.get("/plants", async (req, res) => {
-      const result = await plantsCollection.find().toArray()
-      res.send(result)
-    })
+      const result = await plantsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get a single data from database
+    app.get("/plants/:id", async (req, res) => {
+      const { id } = req.params
+      const query = {_id: new ObjectId(id)}
+      const result = await plantsCollection.findOne(query)
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
